84c1c8e18f9d48a0e8f4cc7e4cf326d83e789bd1
BradlyVinceGorgonio <118922196+BradlyVinceGorgonio@users.noreply.github.com>
2023-06-03 21:17:20 +0800

    EasyBook

---
+++ app/src/main/java/com/example/easybook/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.easybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //bago page
+        //sana marecieve mo
+        //comit and push ko na
+    }
+}
