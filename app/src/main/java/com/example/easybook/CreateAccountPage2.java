20771b8446156628ae3fd87ae93cae7ab1125cbb
BradlyVinceGorgonio <118922196+BradlyVinceGorgonio@users.noreply.github.com>
2023-06-03 23:05:39 +0800

    EasyBook

---
+++ app/src/main/java/com/example/easybook/CreateAccountPage2.java
@@ -0,0 +1,14 @@
+package com.example.easybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccountPage2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_page2);
+    }
+}
