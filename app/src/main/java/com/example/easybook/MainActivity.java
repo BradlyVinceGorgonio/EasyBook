3b567118a97b71501c09aa818ccdb84a0884f3e1
DanielGals <janjangallaron@gmail.com>
2023-06-04 00:41:42 +0800

    it will now work

--- app/src/main/java/com/example/easybook/MainActivity.java
+++ app/src/main/java/com/example/easybook/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, CreateAccountPage.class);
+                startActivity(intent);
             }
         });
 
