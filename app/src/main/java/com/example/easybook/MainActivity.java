ed4c7e0fd4d3ca6c6b29360c1543e6a0c35b36da
DanielGals <janjangallaron@gmail.com>
2023-06-03 22:33:16 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/easybook/MainActivity.java
+++ app/src/main/java/com/example/easybook/MainActivity.java
@@ -1,28 +1,101 @@
 package com.example.easybook;
 
+import static com.google.firebase.inappmessaging.internal.Logging.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
     EditText emailLgn, passLgn;
-    Button loginBtn, registerBtn;
+    Button loginBtn, createAcc;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
 
+        emailLgn = (EditText) findViewById(R.id.emailLgn);
+        passLgn = (EditText) findViewById(R.id.passwordLgn);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        createAcc = (Button) findViewById(R.id.createAccBtn);
 
 
+        loginBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String Semail = emailLgn.getText().toString();
+                String Spass = passLgn.getText().toString();
+                signIn(Semail, Spass);
+            }
+        });
+
+    }
+
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void updateUI(FirebaseUser user)
+    {
+        Intent intent = new Intent(MainActivity.this, LoginPage.class);
+        //Add constructor to LoginBtn class to place infos
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void reload()
+    {
+        Intent intent = new Intent(MainActivity.this, LoginPage.class);
+        startActivity(intent);
     }
 }
