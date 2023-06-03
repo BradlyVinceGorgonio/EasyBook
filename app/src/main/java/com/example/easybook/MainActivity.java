fca3f14ced9e5823b21885b632136edc20e71cdd
DanielGals <janjangallaron@gmail.com>
2023-06-03 22:06:57 +0800

    Added Dependencies and Internet Connection. Database aswell

--- app/src/main/java/com/example/easybook/MainActivity.java
+++ app/src/main/java/com/example/easybook/MainActivity.java
@@ -3,17 +3,26 @@ package com.example.easybook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    EditText emailLgn, passLgn;
+    Button loginBtn, registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //bago page
-        //sana marecieve mo
-        //comit and push ko na
-        //daniel pushing ups betlogies
+        mAuth = FirebaseAuth.getInstance();
+
+
+
     }
 }
