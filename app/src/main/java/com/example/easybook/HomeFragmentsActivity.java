52f69c13417ec7a033585843daae06787c901057
DanielGals <janjangallaron@gmail.com>
2023-06-05 02:14:07 +0800

    nonsense

--- app/src/main/java/com/example/easybook/HomeFragmentsActivity.java
+++ app/src/main/java/com/example/easybook/HomeFragmentsActivity.java
@@ -1,18 +1,65 @@
 package com.example.easybook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 
 public class HomeFragmentsActivity extends AppCompatActivity
 {
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    ChatFragment chatFragment = new ChatFragment();
+    MailFragment mailFragment = new MailFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MyApp", "Bago mag crash");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_fragments);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        Log.d("MyApp", "getSupport");
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_layout, homeFragment).commit();
+
+
+        /*
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.emailButton);
+        badgeDrawable.setVisible(true);
+        badgeDrawable.setNumber(3);
+        */
+
+
+
+
+        Log.d("MyApp", "Na mag crash");
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.homeButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, homeFragment).commit();
+                    return true;
+                } else if (itemId == R.id.messageButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, chatFragment).commit();
+                    return true;
+                } else if (itemId == R.id.emailButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, mailFragment).commit();
+                    return true;
+                } else if (itemId == R.id.profileButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, profileFragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
 
 
     }
@@ -36,53 +83,6 @@ public class HomeFragmentsActivity extends AppCompatActivity
 
 
 /*
-    BottomNavigationView bottomNavigationView;
-    HomeFragment homeFragment = new HomeFragment();
-    ChatFragment chatFragment = new ChatFragment();
-    MailFragment mailFragment = new MailFragment();
-    ProfileFragment profileFragment = new ProfileFragment();
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_fragments);
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, homeFragment).commit();
-
-        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.emailButton);
-        badgeDrawable.setVisible(true);
-        badgeDrawable.setNumber(3);
-
-
-
-
-
-
-
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                switch (item.getItemId())
-                {
-                    case R.id.homeButton:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,homeFragment).commit();
-                        return true;
-
-                    case R.id.messageButton:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,chatFragment).commit();
-                        return true;
-                    case R.id.emailButton:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,mailFragment).commit();
-                        return true;
-                    case R.id.profileButton:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,mailFragment).commit();
-                        return true;
-                }
-
-                return false;
-            }
-        });
         */
 
