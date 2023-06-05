f03d6a5f025a2b7e99a82d18ecfe9abef5acf0ed
DanielGals <janjangallaron@gmail.com>
2023-06-06 00:59:54 +0800

    Dsiplayusername Fixed

--- app/src/main/java/com/example/easybook/HomeFragment.java
+++ app/src/main/java/com/example/easybook/HomeFragment.java
@@ -2,23 +2,36 @@ package com.example.easybook;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import android.widget.TextView;
 
 
 public class HomeFragment extends Fragment {
-
+    private FirebaseAuth mAuth;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
         CardView council = view.findViewById(R.id.counselImageButton);
@@ -33,7 +46,53 @@ public class HomeFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
+
+
+
+
+
+        TextView displayUser = view.findViewById(R.id.displayUserName);
+
+        mAuth = FirebaseAuth.getInstance();
+        displayUserName(view);
+
         return view;
+
+    }
+    public void displayUserName(View view)
+    {
+
+        //displayName = (EditText) findViewById(R.id.welcomeUser);
+
+
+
+        TextView displayUser = view.findViewById(R.id.displayUserName);
+        FirebaseUser user = mAuth.getCurrentUser();
+        String uid = user.getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference userRef = db.collection("costumer").document(uid);
+        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // DocumentSnapshot contains the data
+                        String username = document.getString("username");
+                        Log.d("TAG", "Name: " + username);
+                        displayUser.setText("Welcome to Easy Booking " + username);
+
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+
 
     }
 }
