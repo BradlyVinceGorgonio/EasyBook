c1b554069664f1db514c06f1955b8acc348bb202
DanielGals <janjangallaron@gmail.com>
2023-06-09 18:08:42 +0800

    Added info gather firebase

--- app/src/main/java/com/example/easybook/TrainerDescriptionFragment.java
+++ app/src/main/java/com/example/easybook/TrainerDescriptionFragment.java
@@ -2,6 +2,7 @@ package com.example.easybook;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.text.TextUtils;
@@ -11,6 +12,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -29,6 +34,41 @@ public class TrainerDescriptionFragment extends Fragment
 
         fetchTrainerDataFromFirestore(trainerId);
 
+        //Book now Button
+        Button bookNowBtn = (Button) view.findViewById(R.id.booknowBtn);
+        bookNowBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                CollectionReference customerCollectionRef = db.collection("customer");
+                String currentUserId = firebaseAuth.getCurrentUser().getUid();
+
+                customerCollectionRef.document(currentUserId).get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                if (documentSnapshot.exists()) {
+                                    String name = documentSnapshot.getString("name");
+                                    String uid = documentSnapshot.getString("uid");
+                                    long timestamp = System.currentTimeMillis();
+
+                                    // Do something with the retrieved data (name, uid, timestamp)
+                                } else {
+                                    // Document does not exist
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Error retrieving the document
+                            }
+                        });
+            }
+        });
 
 
         return view;
