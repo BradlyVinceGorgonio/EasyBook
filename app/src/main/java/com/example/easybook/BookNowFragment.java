d721d85b3fda6709b157995dfa990e786f8f5dc6
DanielGals <janjangallaron@gmail.com>
2023-06-10 19:56:34 +0800

    Displayed the Schedule time and date in booknowFragment

--- app/src/main/java/com/example/easybook/BookNowFragment.java
+++ app/src/main/java/com/example/easybook/BookNowFragment.java
@@ -1,5 +1,6 @@
 package com.example.easybook;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -20,14 +25,18 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BookNowFragment extends Fragment {
 
     Button confirmBook;
+    CheckBox Beginner,Experienced,Intermediate,Advanced;
+    private RadioGroup radioGroup;
 
-    
+
     private String trainerId;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -36,6 +45,49 @@ public class BookNowFragment extends Fragment {
 
         confirmBook = (Button) view.findViewById(R.id.confirmBook);
         //Initialize mo muna
+        radioGroup = view.findViewById(R.id.radioGroup);
+
+
+
+
+
+
+        // Get Information from trainer
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        if (getArguments() != null) {
+            trainerId = getArguments().getString("trainerId");
+        }
+        Log.d("Taggers", "trainer id " + trainerId);
+
+        db.collection("trainer").document(trainerId)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        Object scheduleDaysObj = documentSnapshot.get("schedule_day");
+                        if (scheduleDaysObj instanceof List) {
+                            List<String> scheduleDays = (List<String>) scheduleDaysObj;
+                            // Do something with the scheduleDays list
+                            Log.d("Schedule", scheduleDays.toString());
+                            DisplayRadioButton(scheduleDays, radioGroup, requireContext());
+                        } else {
+                            // The schedule_day field is null or not found, or it's not of type List<String>
+                        }
+
+
+
+
+                    } else {
+                        // The document does not exist
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    // Handle any errors that occur while fetching the document
+                });
+
+
+
+
+
         confirmBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
@@ -78,12 +130,45 @@ public class BookNowFragment extends Fragment {
             }
         });
 
-
-
-
-
         return view;
     }
+
+
+
+    public void DisplayRadioButton(List<String> scheduleDays, RadioGroup radioGroup, Context context) {
+        // Create radio buttons dynamically based on the number of strings in the list
+        for (int i = 0; i < scheduleDays.size(); i++) {
+            String option = scheduleDays.get(i);
+
+            RadioButton radioButton = new RadioButton(context);
+            radioButton.setText(option);
+            radioButton.setId(i); // Set a unique ID for each radio button
+
+            radioGroup.addView(radioButton);
+        }
+
+        // Set the OnCheckedChangeListener for the RadioGroup
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Retrieve the selected radio button
+                RadioButton selectedRadioButton = group.findViewById(checkedId);
+
+                // Get the text value of the selected radio button
+                String selectedOption = selectedRadioButton.getText().toString();
+
+                // Store the selectedOption in the chosen database
+                // Implement the logic to store the value in your chosen database
+            }
+        });
+    }
+
+
+
+
+
+
+
 
     public void addToBookingRequestToTrainer(String trainerDocumentId, String name, String uid)
     {
